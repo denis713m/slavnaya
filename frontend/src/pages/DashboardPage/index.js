ba78a2012c8e42e2b382745601e754c43b17c5fa
User Name <artur.it2914@gmail.com>
2020-03-16 21:26:15 +0200

    Create: dashboard page navigation skeleton

--- frontend/src/pages/DashboardPage/index.js
+++ frontend/src/pages/DashboardPage/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React                      from 'react';
+import { NavLink, Switch, Route } from 'react-router-dom';
+
+const DashboardNav = () => {
+  return (
+    <ul>
+      <li><NavLink activeClassName={} to={'/dashboard/link1'}>Link number #1</NavLink></li>
+      <li><NavLink activeClassName={}  to={'/dashboard/link2'}>Link number #2</NavLink></li>
+      <li><NavLink to={'/dashboard/link3'}>Link number #3</NavLink></li>
+    </ul>
+  );
+};
 
 const DashboardPage = () => {
   return (
-    <h1>Dashboard!</h1>
+    <>
+      <DashboardNav/>
+      <Switch>
+        <Route path="/dashboard/link1">
+          <h1>1</h1>
+        </Route>
+        <Route path="/dashboard/link2">
+          <h1>2</h1>
+        </Route>
+        <Route path="/dashboard/link3">
+          <h1>3</h1>
+        </Route>
+      </Switch>
+    </>
   );
 };
 
