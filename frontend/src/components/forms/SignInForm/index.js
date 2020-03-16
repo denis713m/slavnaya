1217110952bfe6012c6b855d43a1f9f855cd6a8f
User Name <artur.it2914@gmail.com>
2020-03-16 15:17:29 +0200

    Refactor: Sign in form example

--- frontend/src/components/forms/SignInForm/index.js
+++ frontend/src/components/forms/SignInForm/index.js
@@ -1,48 +1,61 @@
-import React                       from 'react';
-import { Form, withFormik, Field } from 'formik';
-import * as Yup                    from 'yup';
-import Input                       from '../Input';
-import Label                       from '../Label';
-import StyledErrorMessage          from '../StyledErrorMessage';
+import React, { useState }                     from 'react';
+import { Form, withFormik, Field, FieldArray } from 'formik';
+import * as Yup                                from 'yup';
+import Input                                   from '../Input';
+import Label                                   from '../Label';
+import StyledErrorMessage                      from '../StyledErrorMessage';
+import styles                                  from './SignInForm.module.scss';
+import Button                                  from '../Button';
 
-import styles from './SignInForm.module.scss';
-import Button from '../Button';
 
 const SignInForm = (props) => {
 
   const { values, isSubmitting } = props;
+  const [fields, setFields] = useState( [
+                                          {
+                                            name: 'email',
+                                            type: 'email',
+                                            placeholder: 'Email Address',
+                                          },
+                                          {
+                                            name: 'password',
+                                            type: 'password',
+                                            placeholder: 'Password',
+                                          },
+                                        ] );
 
-  const fieldRender = (name, type, placeholder) => {
-    return (
-      <Field name={name} value={values[name]}>
+  const renderFields = () => {
+
+    return fields.map( ({ name, ...rest }) => (
+      <Field key={name} name={name} value={values[name]}>
         {
           fieldProps => (
             <Label className={styles.fieldWrapper}>
-              <Input placeholder={placeholder} type={type} {...fieldProps}/>
+              <Input {...rest} {...fieldProps}/>
               <StyledErrorMessage className={styles.errorWrapper} name={fieldProps.field.name}/>
             </Label>
           )
         }
-      </Field>
-    );
+      </Field>) );
   };
 
   return (
     <Form className={styles.form}>
       {
-        fieldRender( 'email', 'email', 'Email address' )
+        renderFields()
       }
-      {
-        fieldRender( 'password', 'password', 'Password' )
-      }
-      <Button className={styles.submitButton} disabled={isSubmitting} type='submit'>login</Button>
+      <Button onClick={props.submitForm} className={styles.submitButton} disabled={isSubmitting}
+              type='submit'>login</Button>
     </Form>
   );
 };
 
 export default withFormik( {
                              handleSubmit: (values, formikBag) => { alert( JSON.stringify( values, null, 4 ) ); },
-                             mapPropsToValues: () => ({ email: '', password: '' }),
+                             mapPropsToValues: () => ({
+                               email: '',
+                               password: '',
+                             }),
                              validationSchema: Yup.object( {
                                                              email: Yup.string().email().required(),
                                                              password: Yup.string().required(),
